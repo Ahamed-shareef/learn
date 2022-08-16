ece00bb3760d55cd4ca97afbc7897dc3545bfa20
Muhammad Yousif <m.alshf3ee@gmail.com>
2022-08-16 23:42:59 +0300

    Design Perfection

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -7,84 +7,81 @@ import 'package:learn/constants/route_names.dart';
 
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.background,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Center(
-            child: CircleAvatar(
-              radius: 100,
+      body: SafeArea(
+        child: ListView(
+          children: [
+            const SizedBox(height: 20),
+            const CircleAvatar(
+              radius: 70,
               backgroundImage: AssetImage("images/login.jpg"),
             ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Card(
-            clipBehavior: Clip.antiAliasWithSaveLayer,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            margin: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-            color: const Color.fromARGB(255, 235, 199, 156),
-            child: Container(
-              padding: const EdgeInsets.all(15),
-              child: Form(
-                  child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
+            const SizedBox(height: 20),
+            Card(
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+              margin: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+              color: AppColors.background,
+              child: Container(
+                padding: const EdgeInsets.all(15),
+                child: Form(
+                    child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
                         hintText: "username",
                         prefixIcon: const Icon(Icons.person),
-                        border: OutlineInputBorder(borderSide: const BorderSide(width: 1), borderRadius: BorderRadius.circular(40))),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        hintText: "password",
-                        prefixIcon: const Icon(Icons.person),
-                        border: OutlineInputBorder(borderSide: const BorderSide(width: 1), borderRadius: BorderRadius.circular(40))),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.all(10),
-                    child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                          primary: Colors.brown,
-                          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                        ),
-                        onPressed: () {
-                          Navigator.of(context).pushReplacementNamed(AppRouteNames.home);
-                        },
-                        child: const Text(
-                          "تسجيل الدخول ",
-                          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                        )),
-                  ),
-                  const SizedBox(
-                    height: 50,
-                  ),
-                  Row(
-                    children: const [
-                      InkWell(
-                        child: Text(
-                          "اضغط هنا ",
-                          style: TextStyle(color: Colors.blue, fontSize: 20),
+                        border: OutlineInputBorder(
+                          borderSide: const BorderSide(width: 1),
+                          borderRadius: BorderRadius.circular(40),
                         ),
                       ),
-                      Text("اذا كنت لا تملك حساب "),
-                    ],
-                  )
-                ],
-              )),
-            ),
-          )
-        ],
+                    ),
+                    const SizedBox(height: 20),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: "password",
+                        prefixIcon: const Icon(Icons.person),
+                        border: OutlineInputBorder(
+                          borderSide: const BorderSide(width: 1),
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      margin: const EdgeInsets.all(10),
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                            primary: Colors.brown,
+                            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                          ),
+                          onPressed: () => Navigator.of(context).pushReplacementNamed(AppRouteNames.home),
+                          child: const Text(
+                            "تسجيل الدخول ",
+                            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                          )),
+                    ),
+                    const SizedBox(height: 50),
+                    Row(
+                      children: const [
+                        InkWell(
+                          child: Text("اضغط هنا ", style: TextStyle(color: AppColors.primaryColor, fontSize: 18)),
+                        ),
+                        Text("اذا كنت لا تملك حساب "),
+                      ],
+                    )
+                  ],
+                )),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
