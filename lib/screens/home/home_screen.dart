4e1e3d0f144541e1c997ca2918f3378f2bc9d14b
Ahmed Sharif <wshareef24@gmail.com>
2022-08-19 10:52:13 +0200

    New design in trial

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:learn/constants/colors.dart';
+import 'package:learn/models/categories.dart';
 import 'package:learn/screens/home/widgets/dishes_search.dart';
 import 'package:learn/screens/home/widgets/home_screen_design.dart';
 
@@ -12,29 +13,72 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          //! title: const Center(child: Text("Categories")),
+    return DefaultTabController(
+      length: 9,
+      child: Scaffold(
           backgroundColor: Colors.white,
-          foregroundColor: AppColors.primaryColor,
-          elevation: 0,
-          // shape: const Border(bottom: BorderSide(color: AppColors.primaryColor)),
-          actions: [
-            const SizedBox(width: 5),
-            IconButton(
-              icon: const Icon(Icons.search, size: 22),
-              onPressed: () => showSearch(
-                  context: context, delegate: DishesSearch()), //* Search View
-            ),
-            IconButton(
-              icon: const Icon(Icons.shopping_bag_outlined, size: 22),
-              onPressed: () {},
-            ),
-            const SizedBox(width: 5),
-          ],
-        ),
-        //! drawer: const Drawer(),
-        body: const HomeScreenDesign());
+          appBar: AppBar(
+            bottom: TabBar(tabs: [
+              Tab(
+                child: Text(category[0].name),
+              ),
+              Tab(
+                child: Text(category[1].name),
+              ),
+              Tab(
+                child: Text(category[2].name),
+              ),
+              Tab(
+                child: Text(category[3].name),
+              ),
+              Tab(
+                child: Text(category[4].name),
+              ),
+              Tab(
+                child: Text(category[5].name),
+              ),
+              Tab(
+                child: Text(category[6].name),
+              ),
+              Tab(
+                child: Text(category[7].name),
+              ),
+              Tab(
+                child: Text(category[8].name),
+              ),
+            ]),
+            //! title: const Center(child: Text("Categories")),
+            backgroundColor: Colors.white,
+            foregroundColor: AppColors.primaryColor,
+            elevation: 0,
+            // shape: const Border(bottom: BorderSide(color: AppColors.primaryColor)),
+            actions: [
+              const SizedBox(width: 5),
+              IconButton(
+                icon: const Icon(Icons.search, size: 22),
+                onPressed: () => showSearch(
+                    context: context, delegate: DishesSearch()), //* Search View
+              ),
+              IconButton(
+                icon: const Icon(Icons.shopping_bag_outlined, size: 22),
+                onPressed: () {},
+              ),
+              const SizedBox(width: 5),
+            ],
+          ),
+          //! drawer: const Drawer(),
+          // ignore: prefer_const_literals_to_create_immutables
+          body: const TabBarView(children: [
+            HomeScreenDesign(),
+            HomeScreenDesign(),
+            HomeScreenDesign(),
+            HomeScreenDesign(),
+            HomeScreenDesign(),
+            HomeScreenDesign(),
+            HomeScreenDesign(),
+            HomeScreenDesign(),
+            HomeScreenDesign(),
+          ])),
+    );
   }
 }
